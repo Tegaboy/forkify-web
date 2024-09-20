78aaaa103349c31028908503004fb0f99988b917
Tegaboy <johnokandejio@gmail.com>
2024-09-20 12:47:07 +0100

    Corrected the build issue

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 12;
-export const API_KEY = '18b68112-a33f-4e98-8ad4-74ef8f9945dd';
+export const API_KEY = '4112a346-9619-422f-93c4-44cd94cc8589';
 export const MODAL_TIME_OUT = 2.5;
